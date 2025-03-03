a1fe196ae52efac9724e82e25a43479d334bac6a
FranInfante <infante.portero@gmail.com>
2025-03-03 18:38:09 +0100

    init angular

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './modules/tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: TasksComponent}
+];
---
+++ angular-frontend/src/app/shared/services/task-service.service.ts
@@ -0,0 +1,31 @@
+// src/app/task.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../interfaces/Task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:8000/api/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+  
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  }
+  
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
