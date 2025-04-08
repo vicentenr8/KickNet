ce36bec4e55f786c355a1d0e959b62d99de5c0cc
Jesús Gómez Beltrán <jgomezbeltran88@gmail.com>
2025-04-08 14:56:57 +0200

    Se realiza draf 3 y se actualizan estilos del landing page y de task

--- angular-frontend/src/app/modules/tasks/tasks.component.ts
+++ angular-frontend/src/app/modules/tasks/tasks.component.ts
@@ -14,6 +14,7 @@ import { RouterModule } from '@angular/router';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
+  
 
   constructor(private taskService: TaskService) {}
 
