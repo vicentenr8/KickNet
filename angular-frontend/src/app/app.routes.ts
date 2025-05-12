589df72573086c1b3ec391b058e26fdb78e2ffa2
vicentenr8 <vicentenr8@gmail.com>
2025-05-12 10:55:33 +0200

    Inicio-Proyecto

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './modules/tasks/tasks.component';
-import { LandingComponent } from './modules/landing/landing.component';
 
 export const routes: Routes = [
-    {path: "", component: LandingComponent},
-    {path: "tasks", component: TasksComponent}
+    
 ];
