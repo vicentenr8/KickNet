b39c02a3ef2ba04c58c302342ba864b2a1a4c801
vicentenr8 <vicentenr8@gmail.com>
2025-05-27 19:23:10 +0200

    migracion

--- symfony-backend/app/migrations/Version20250527171235.php
+++ symfony-backend/app/migrations/Version20250527171235.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250302203001 extends AbstractMigration
+final class Version20250527171235 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,12 @@ final class Version20250302203001 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE task_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE task (id INT NOT NULL, title VARCHAR(255) DEFAULT NULL, completed VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE task_id_seq CASCADE');
-        $this->addSql('DROP TABLE task');
     }
 }
